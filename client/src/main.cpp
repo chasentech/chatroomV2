66f3f0e1b5fb431ec92670f6a4a4c854ff7e0b77
zcl <xiyezcl@foxmail.com>
2019-08-17 23:43:34 +0800

    Complete chat functionin in admin

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -18,14 +18,155 @@ using namespace std;
 #define MAX_LEN 256
 #define LISTEN_PORT 8888
 
+typedef enum MsgType
+{
+    READY_TYPE = 1,
+    CHAT_TYPE,
+}MsgType;
 
-bool is_running = true;
+typedef enum ReadyMsgType
+{
+    READY_MSG_SIGN_IN = 1,
+    READY_MSG_SIGN_ON = 2,
+    READY_MSG_ADMIN = 3,
+    READY_MSG_TEST = 4,
+}ReadyMsgType;
+
+typedef enum ChatMsgType
+{
+    CHAT_MSG_NORMAL = 1,
+    CHAT_MSG_GET_ID = 2,
+}ChatMsgType;
+
+static void printf_menu()
+{
+    printf("Please select item:\n");
+    printf("    1. sign in.\n");
+    printf("    2. sign up.\n");
+    printf("    3. administrator.\n");
+    printf("    4. set.\n");
+    printf("    5. exit.\n");
+}
+
+//是否存在'\n'，并将回车改为'\0'
+int is_enter(char *p)
+{
+    while (*p != '\0')
+    {
+        if (*p == '\n')
+        {
+            *p = '\0';
+            return 1;
+        }
+
+        p++;
+    }
+    return 0;
+}
+
+void input(char *buf, int len)
+{
+    int flag = 0;
+    while (1)
+    {
+        char temp[20] = {0};
+        if (flag == 0)
+        {
+            flag = 1;
+            fgets(buf, len, stdin);
+            if (is_enter(buf) == 1) return;
+        }
+        else
+        {
+            fgets(temp, sizeof(temp), stdin);
+            if (is_enter(temp) == 1) return;
+        }
+    }
+}
+
+void encode(char *buf, MsgType type1, ChatMsgType type2,
+            int to, char *msg)
+{
+    int len = 0;
+    char *p_buf = buf;
+    *p_buf = '#'; p_buf++; len++;  //head
+    *p_buf = 'n'; p_buf++; len++;  //n,长度
+    *p_buf = type1; p_buf++; len++; //type2
+    *p_buf = type2; p_buf++; len++; //type2
+    int n2 = sizeof(to);           //to
+    memcpy(p_buf, &to, n2); p_buf += n2; len += n2;
+    *p_buf = 1; p_buf++; len++;    //msg n
+    strcpy(p_buf, msg); p_buf += strlen(msg); len += strlen(msg);
+    *p_buf = '|'; p_buf++; len++;  //
+    *p_buf = '$'; p_buf++; len++;
+
+    buf[1] = len;
+    if (len >= MAX_LEN)
+    {
+        printf("out of len!");
+        memset(buf, 0, MAX_LEN);
+        return;
+    }
+}
+
+void printf_buf(char *buf)
+{
+    printf("[%c][%d], t1:[%d], t2:[%d]\n",
+           buf[0], buf[1], buf[2], buf[3]);
+    int to = -1;
+    int n = sizeof(to);
+    memcpy(&to, (const char *)(buf+4), n);
+    printf("to: [%d]\n", to);
+    printf("nmsg: [%d]\n", buf[4+n]);
+    printf("msg: [%s]\n", buf+4+n+1);
+}
+
+void module_admin(int sockfd)
+{
+    char buf[MAX_LEN] = {0};
+    printf("in admin module, input quit to exit\n");
+
+    //get list of ID
+    encode(buf, CHAT_TYPE, CHAT_MSG_GET_ID, 0, (char *)" ");
+    printf_buf(buf);
+    Write(sockfd, buf, buf[1]);
+    printf("len=%d\n", buf[1]);
+    printf("Please friend's ID:\n");
+
+    char to_temp[10] = {0};
+    input(to_temp, 10);
+    if (atoi(to_temp) <= 0) //default is self
+    {
+        sprintf(to_temp, "%d", sockfd);
+    }
+
+    printf("your input is %s\n", to_temp);
+
+
+    while(1)
+    {
+        printf("Input: ");
+        char buf_msg[200] = {0};
+        input(buf_msg, 200);
+        if (!strncmp(buf_msg, "quit", 4)) return;
+
+
+        encode(buf, CHAT_TYPE, CHAT_MSG_NORMAL,
+                atoi(to_temp), buf_msg);
+
+        printf_buf(buf);
+        printf("len = %d\n", buf[1]);
+
+        Write(sockfd, buf, buf[1]);
+        bzero(buf, MAX_LEN);
+    }
+}
 
 void signalstop(int sign_no)
 {
     if(sign_no == SIGINT)
     {
-        printf("Please input quit to exit!\n");
+        printf("Please exit in the right way!\n");
     }
 }
 
@@ -44,9 +185,6 @@ void signalchld(int sign_no)
     }
 }
 
-
-
-
 int main()
 {
     signal(SIGINT, signalstop);
@@ -61,7 +199,6 @@ int main()
 
     Connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
-    char buf[MAX_LEN];
     pid_t pid = fork();
     if (pid < 0)
     {
@@ -70,22 +207,35 @@ int main()
     }
     if (pid == 0) //child process
     {
-        while (fgets(buf, MAX_LEN, stdin) != NULL)
+        printf("-----Welcome to chatroom-----\n");
+        while (1)
         {
-            if (!strncmp(buf, "quit", 4))
+            printf_menu();
+            printf("Input: ");
+
+            //input select
+            bool is_break = false;
+            char buf_select[5] = {0};
+            input(buf_select, 2);
+            switch (buf_select[0])
             {
-                break;
+                case '1': printf("enter sign in\n"); break;
+                case '2': printf("enter sign up\n"); break;
+                case '3': printf("enter administrator\n"); module_admin(sockfd); break;
+                case '4': printf("enter set\n"); break;
+                case '5': printf("enter exit\n"); is_break = true; break;
+                default: printf("invalid input\n"); break;
             }
 
-            Write(sockfd, buf, strlen(buf));
-            bzero(buf, strlen(buf));
+            if (is_break) break;
         }
     }
     else //main process
     {
+        char buf_rece[MAX_LEN] = {0};
         while (1)
         {
-            int n = Read(sockfd, buf, MAX_LEN);
+            int n = Read(sockfd, buf_rece, MAX_LEN);
             if (n == 0)
             {
                 printf("the other side has been closed.\n");
@@ -93,12 +243,13 @@ int main()
             }
             else
             {
-                Write(STDOUT_FILENO, buf, n);
-                bzero(buf, n);
+                Write(STDOUT_FILENO, buf_rece, n);
+                bzero(buf_rece, n);
             }
         }
 
         Close(sockfd);
+        wait(NULL);
         printf("main exit!\n");
 
     }
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include <stdio.h>
 #include <sys/socket.h>
@@ -18,16 +19,174 @@ using namespace std;
 #define LISTEN_PORT 8888
 #define MAX_CLI_NUM 100
 
-bool is_running = true;
+typedef enum CliType
+{
+    CLI_TYPE_ADMIN,
+    CLI_TYPE_NORMAL,
+}CliType;
+
+typedef struct CliInfo
+{
+    char IP[INET_ADDRSTRLEN]; //INET_ADDRSTRLEN = 16
+    int port;
+    int sockfd;
+    CliType type; //administrator normal
+}CliInfo;
+
+
+typedef enum MsgType
+{
+    READY_TYPE = 1,
+    CHAT_TYPE,
+}MsgType;
+
+typedef enum ReadyMsgType
+{
+    READY_MSG_SIGN_IN = 1,
+    READY_MSG_SIGN_ON = 2,
+    READY_MSG_ADMIN = 3,
+    READY_MSG_TEST = 4,
+}ReadyMsgType;
+
+typedef enum ChatMsgType
+{
+    CHAT_MSG_NORMAL = 1,
+    CHAT_MSG_GET_ID = 2,
+}ChatMsgType;
+
+
+vector<CliInfo> g_vec_cli_info;
+
+
+void print_cli_info(vector<CliInfo> &vec)
+{
+    cout << "********<<cli_info<<********" << endl;
+    cout << "client number: " << vec.size() << endl;
+    for (unsigned int i = 0; i < vec.size(); i++)
+    {
+        cout << "    cli[" << i << "]_IP:     " << vec[i].IP << endl
+             << "    cli[" << i << "]_port:   " << vec[i].port << endl
+             << "    cli[" << i << "]_sockfd: " << vec[i].sockfd << endl
+             << "    cli[" << i << "]_type:   " << vec[i].type << endl;
+        cout << "----------------------------" << endl;
+    }
+}
+
+void vec_rm_value(vector<CliInfo> &vec, int value)
+{
+    vector<CliInfo>::iterator it;
+    for (it = vec.begin(); it != vec.end(); )
+    {
+        if (it->sockfd == value)
+            it = vec.erase(it);
+        else it++;
+    }
+}
+
+void return_fd(vector<CliInfo> &vec, int fd)
+{
+    vector<CliInfo>::iterator it;
+    for (it = vec.begin(); it != vec.end(); it++)
+    {
+        char buf[30] = {0};
+        if (it->sockfd == fd)
+            sprintf(buf, "********%d<--[you]\n", it->sockfd);
+        else sprintf(buf, "********%d\n", it->sockfd);
+        Write(fd, buf, strlen(buf));
+    }
+}
+
+void printf_buf(char *buf)
+{
+    printf("[%c][%d], t1:[%d], t2:[%d]\n",
+           buf[0], buf[1], buf[2], buf[3]);
+    int to = -1;
+    int n = sizeof(to);
+    memcpy(&to, (const char *)(buf+4), n);
+    printf("to: [%d]\n", to);
+    printf("nmsg: [%d]\n", buf[4+n]);
+    printf("msg: [%s]\n", buf+4+n+1);
+}
+
+void decode_ready(char *buf, int sockfd)
+{
+
+}
+void decode_chat(char *buf, int sockfd)
+{
+    if (buf[3] == CHAT_MSG_NORMAL)
+    {
+        int from = sockfd;
+        int to = -1;
+        int n = sizeof(to);
+        memcpy(&to, (const char *)(buf+4), n);
+
+        printf("[%d]-->[%d]\n", from, to);
+        printf("nmsg: [%d]\n", buf[4+n]);
+
+        if (buf[4+n] >= 10)
+        {
+            printf("nmsg is too long!\n");
+            return;
+        }
+
+        //msg number max is 10
+        char *argv_t[10] = {NULL};
+        char *p = buf+4+n+1;
+
+        int j = 0;
+        argv_t[j++] = p;
+        while (*p != '$')
+        {
+            if (*p == '|')
+            {
+                *p = '\0';
+                argv_t[j++] = p + 1;
+            }
+            p++;
+        }
+        *p = '\0';
+
+
+        for (j = 0 ; argv_t[j] != NULL &&
+                (strcmp(argv_t[j], "")) && j < 10; j++)
+        {
+            printf("msg[%d]: [%s]\n", j, argv_t[j]);
+        }
+
+        char send_buf[200] = {0};
+        sprintf(send_buf, "\n[%d]-->[%d]: %s\n", from, to, argv_t[0]);
+        Write(to, send_buf, strlen(send_buf));
+
+    }
+    else if (buf[3] == CHAT_MSG_GET_ID)
+    {
+        return_fd(g_vec_cli_info, sockfd);
+    }
+}
+
+void decode(char *buf, int sockfd)
+{
+    if (buf[0] == '#' && buf[buf[1] - 1] == '$')
+    {
+        printf("rece correct!\n");
+        if (buf[2] == READY_TYPE)
+            decode_ready(buf, sockfd);
+        else if (buf[2] == CHAT_TYPE)
+            decode_chat(buf, sockfd);
+        else printf("rece type error!\n");
+    }
+    else printf("rece error!\n");
+}
 
 
 static int listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
 void signalstop(int signum)
 {
-    is_running = false;
     printf("catch signal!\n");
     Close(listenfd);
+    g_vec_cli_info.clear();
     exit(0);
 }
 
@@ -64,7 +223,7 @@ int main()
     char buf[MAX_LEN] = {0};
     printf("Accepting connections ...\n");
 
-    while (is_running)
+    while (1)
     {
         rset = allset;
         int nready = select(maxfd+1, &rset, NULL, NULL, NULL);
@@ -75,15 +234,21 @@ int main()
 
         if (FD_ISSET(listenfd, &rset)) // new connect arived
         {
-
+            CliInfo cli_info;
             struct sockaddr_in cliaddr;
             socklen_t cliaddr_len = sizeof(cliaddr);
             int connfd = Accept(listenfd,
                          (struct sockaddr *)&cliaddr, &cliaddr_len);
 
+            inet_ntop(AF_INET, &cliaddr.sin_addr, str_IP, sizeof(str_IP));
+            strcpy(cli_info.IP, str_IP);
+            cli_info.port = ntohs(cliaddr.sin_port);
+            cli_info.sockfd = connfd;
+            cli_info.type = CLI_TYPE_NORMAL;
+
             printf("connect from %s at PORT %d\n",
-                inet_ntop(AF_INET, &cliaddr.sin_addr, str_IP, sizeof(str_IP)),
-                ntohs(cliaddr.sin_port));
+                str_IP, ntohs(cliaddr.sin_port));
+
 
             for (i = 0; i < FD_SETSIZE; i++)
             {
@@ -103,7 +268,9 @@ int main()
             if (connfd > maxfd) maxfd = connfd; //maxfd for select
             if (i > maxi) maxi = i;     //maxi in client_fd
 
-            printf("max fd: %d\n", maxi + 1);
+            g_vec_cli_info.push_back(cli_info);
+            print_cli_info(g_vec_cli_info);
+            //printf("max fd: %d\n", maxi + 1);
 
             if (--nready == 0) continue;
         }
@@ -118,22 +285,26 @@ int main()
                 int n = Read(sockfd, buf, MAX_LEN);
                 if (n == 0)
                 {
+                    print_cli_info(g_vec_cli_info);
+                    vec_rm_value(g_vec_cli_info, sockfd);
+                    print_cli_info(g_vec_cli_info);
+
+                    //cout << "client size: " << g_vec_cli_info.size() << endl;
                     printf("the other side has been closed.\n");
                     Close(sockfd);
                     FD_CLR(sockfd, &allset);
                     client_fd[i] = -1;
                 } else
                 {
-                    printf("received buf: %s", buf);
+                    //printf("received buf: %s", buf);
 
                     //do something
                     //...
-
-                    Write(sockfd, buf, n);
+                    decode(buf, sockfd);
                     memset(buf, 0, n);
                 }
 
-                if (--nready == 0) break; //notice maxi and nready relationship
+                if (--nready == 0) break; //notice maxi and nready relationship, can early exit
             }
         }
     }
